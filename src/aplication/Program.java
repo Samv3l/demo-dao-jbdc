077323bd5bbc2c7f55dbc0a7ea800da106231eec
Samuel Bento <samuel94bento@gmail.com>
2019-09-20 21:01:29 +0100

    FindAll

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,7 +1,10 @@
 package aplication;
 
+import java.util.List;
+
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.enteties.Department;
 import model.enteties.Seller;
 
 public class Program {
@@ -12,7 +15,23 @@ public class Program {
 		System.out.println("----- Tste Seller FindById ------");
 		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
-
+		
+		System.out.println("----- Teste Seller FindByDepartment------");
+		Department department = new Department(2, null);
+		List<Seller> list = sellerDao.fingByDepartment(department);
+		
+		for (Seller obj : list) {
+			System.out.println(obj);
+		}
+			
+		System.out.println("----- Teste Seller FindAll------");
+		list = sellerDao.findAll();
+		
+		for (Seller obj : list) {
+			System.out.println(obj);
+		}
+			
+		
 	}
 
 }
