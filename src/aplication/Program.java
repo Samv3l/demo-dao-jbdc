53094c306f2e0c4d7f2035a0561bcc0c352aa000
Samuel Bento <samuel94bento@gmail.com>
2019-09-20 18:50:08 +0100

    classe departamento

---
+++ src/aplication/Program.java
@@ -0,0 +1,15 @@
+package aplication;
+
+import model.enteties.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Department dp = new Department(1, "ola");
+		
+		System.out.println(dp);
+
+	}
+
+}
