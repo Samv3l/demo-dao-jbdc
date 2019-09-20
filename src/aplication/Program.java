460b4834e0bdaba253546c24edbccf2cfb0772f2
Samuel Bento <samuel94bento@gmail.com>
2019-09-20 19:00:59 +0100

    classe seller

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,6 +1,9 @@
 package aplication;
 
+import java.util.Date;
+
 import model.enteties.Department;
+import model.enteties.Seller;
 
 public class Program {
 
@@ -8,7 +11,9 @@ public class Program {
 		
 		Department dp = new Department(1, "ola");
 		
-		System.out.println(dp);
+		Seller seller = new Seller(2, "Bob", "bob@gmail.com", new Date(), 3000.0, dp);
+		
+		System.out.println(seller);
 
 	}
 
